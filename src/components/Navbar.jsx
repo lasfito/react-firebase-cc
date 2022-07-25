171b93cb37460d69560c711fd8caa518c677fabb
lasfito <hola@lasfi.to>
2022-07-24 22:11:55 -0500

    cambios trigger build

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import logOut from '../functions/logOut';
 function Navbar({ user }) {
 	return (
 		<div className="w-full flex flex-row justify-between px-10 my-5">
-			<p>{user.email}</p>
+			<p>{user.email} Versión 2 LOL</p>
 			<button
 				className="bg-red-300 hover:bg-red-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
 				onClick={logOut}
